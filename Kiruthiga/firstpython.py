a65da4a3e8868424cf1f84a82994c9d3a54ee68b
KiruthigaG <krithiarun3086@gmail.com>
2025-06-05 20:00:41 +0530

    added python variable file reverting the changes of komal which i have updated by mistake

--- Kiruthiga/firstpython.py
+++ Kiruthiga/firstpython.py
@@ -1,1 +1,6 @@
-print("Kiruthiga")
+#1. (a+b)^2 = a^2 + b^2 + 2ab
+a=5
+b=6
+lhs =(a+b)**2
+rhs=(a**2)+(b**2)+2*(a*b)
+print(lhs==rhs)
--- komal/first_program.py
+++ komal/first_program.py
@@ -1,6 +1,2 @@
-#1. (a+b)^2 = a^2 + b^2 + 2ab
-a=5
-b=6
-lhs =(a+b)**2
-rhs=(a**2)+(b**2)+2*(a*b)
-print(lhs==rhs)
+print("hello world")
+# comment

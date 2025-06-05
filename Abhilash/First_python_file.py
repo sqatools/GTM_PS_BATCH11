6de5cc65b35bca0ac74248eb5fdf56881fcd0787
PA0802 <abhilash113131@gmail.com>
2025-06-05 21:02:37 +0530

    Adding files from git

--- Abhilash/First_python_file.py
+++ Abhilash/First_python_file.py
@@ -1,1 +1,43 @@
-print("Hello")
+##### Add two integer values in Python ###
+s1 = 20
+s2 = 30
+
+sum =  s1 + s2
+
+print ( "The sum of integer value is :", sum)
+
+print ("-"*50)
+
+##### subtract two integer values in Python ###
+s1 = 20
+s2 = 30
+
+print ( "subtract of s1-s2  :", s1 - s2)
+
+print ("-"*50)
+
+##### multiply two integer values in Python ###
+s1 = 10
+s2 = 5
+
+print ( "Multiply of s1*s2  :", s1*s2)
+
+print ("-"*50)
+
+##### repeat a given string 5 times in Python ###
+str1 = 'SQATools'
+print(str1*5)
+
+print ("-"*50)
+
+##### Python program to get the Average of given numbers. ###
+### Formula: sum of all the number/ total number ###
+a = 40
+b = 50
+c = 30
+
+s = ((a + b + c)/3)
+
+print ("Average of given numbers :",s)
+
+print ("-"*50)

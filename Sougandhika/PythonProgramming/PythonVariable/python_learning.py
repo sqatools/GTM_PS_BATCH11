52f8cfa8cb6a6ca04c3269b29492611095dca773
sougandhika <hjsougandhika@gmail.com>
2025-06-09 22:15:59 +0530

    Merge branch 'main' of https://github.com/sqatools/GTM_PS_BATCH11

---
+++ Sougandhika/PythonProgramming/PythonVariable/python_learning.py
@@ -0,0 +1,47 @@
+# Python Program to add two integer values.
+a = 12
+b = 14
+print("Add a + b:", a+b)
+
+print("-"*50)
+# Python Program to subtract two integer values.
+a = 12
+b = 14
+print("sub a - b:", a-b)
+
+print("-"*50)
+# Python program to multiply two numbers.
+a = 12
+b = 14
+print("multiply a * b:", a*b)
+
+print("-"*50)
+#Python program to repeat a given string 5 times.
+#Input :str1 = “SQATools”
+#Output :“SQAToolsSQAToolsSQAToolsSQAToolsSQATools”
+str1 = "SQATools"
+a = 5
+print("Result:",str1*a)
+
+print("-"*50)
+#5). Python program to get the Average of given numbers.
+#Formula: sum of all the number/ total number
+#Input:a = 40, b = 50, c = 30,
+#Output : Average = 40
+a = 40
+b = 50
+c = 30
+Average= ((a+b+c)/3)
+print("Average:", (a+b+c)/3)
+
+print("-"*50)
+# Python program to get the median of given numbers.
+#Note: all the numbers should be arranged in ascending order
+#Formula : (n+1)/2
+#n = Number of values
+#Input : [45, 60, 61, 66, 70, 77, 80]
+#Output:  66
+list1 =[45,60,61,66,70,77,80]
+n= len(list1)
+list= (n+1)/2
+print("result:",list)

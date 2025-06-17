64ee6b984b44d99f61b3163f94dff4b346f21ce3
shameema1-job <shameemajob1@gmail.com>
2025-06-17 20:52:12 +0530

    Merge remote-tracking branch 'origin/main'

--- Shameema/first_python_file.py
+++ Shameema/first_python_file.py
@@ -5,24 +5,24 @@ for i in range (6):
     for j in range (5):
         if i == 0:
            if j == 1:
-               print(" ", end=" ")
+               print(" ", end="")
            else:
-               print("*", end=" ")
+               print("*", end="")
         elif i == 1:
            if j in [1,3,4]:
-             print(" ", end=" ")
+             print(" ", end="")
            else:
-             print("*", end=" ")
+             print("*", end="")
         elif i == 2:
            if j in [0,1,2,3,4]:
-             print("*", end=" ")
+             print("*", end="")
         elif i == 3:
            if j in [2,4]:
-             print("*", end=" ")
+             print("*", end="")
            else:
-               print(" ", end=" ")
+               print(" ", end="")
         elif i ==4:
            if j == 3:
-               print(" ", end=" ")
+               print(" ", end="")
            else:
-               print("*", end=" ")
+               print("*", end="")

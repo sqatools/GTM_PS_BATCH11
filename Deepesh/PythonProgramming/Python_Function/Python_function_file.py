3d2dc145b9682fede7447a6fd733f3bb2cc9e0a9
Deepesh <xovek11875@bitvoo.com>
2025-07-03 22:15:45 +0530

    Python Function Session2

--- Deepesh/PythonProgramming/Python_Function/Python_function_file.py
+++ Deepesh/PythonProgramming/Python_Function/Python_function_file.py
@@ -110,6 +110,161 @@ HelloHello
 {'a': 123}
 """
 
-#Q1 . write a python function get max value list
-#Q2.  write a python function get factorial of given number.
-#Q3.  write a python function get sum of all the values in the list.
+# Q1 . write a python function get max value list
+# Q2.  write a python function get factorial of given number.
+# Q3.  write a python function get sum of all the values in the list.
+
+def get_max_value(list1):
+    max_value = max(list1)
+    print("max value :", max_value)
+
+get_max_value([4, 6, 8, 34, 654, 23])
+
+print("_"*50)
+def get_factorial(num):
+    fact =1
+    for i in range(num, 0, -1):
+        fact = fact*i # 1*5 = 5 | 5*4 = 20 | 20*3 = 60 | 60*2 = 120 | 120*1 = 120
+        #print(i)
+    print(f"Factorials of {num}:", fact)
+
+# pass by value
+get_factorial(7)
+
+# pass by reference
+x = 5
+get_factorial(x)
+
+for x in range(6, 10):
+    get_factorial(x)
+
+print("_"*50)
+#################################
+# Q3.  write a python function get sum of all the values in the list.
+
+def get_sum_of_all_values(l1: list):
+    sum = 0
+    for val in l1:
+        sum = sum + val
+
+    print("sum of all values :", sum)
+
+# pass by value
+get_sum_of_all_values([5, 7, 3, 6, 23])
+# sum of all values : 44
+
+# pass by reference
+list_a = [6, 8, 34, 56, 23]
+get_sum_of_all_values(list_a)
+# sum of all values : 127
+
+#get_sum_of_all_values("Hello")
+
+print("_"*40)
+#############################################################
+# **kwargs parameter
+# kwargs parameter accept the values in key value pair
+# kwargs value are store in dictionary format
+
+def get_user_details(**abc):
+    print(abc)
+    for k, v in abc.items():
+        print(k, ":", v)
+
+
+get_user_details(name='Mohan', email='mohan@gmail.com', phone=4534543, address="Mumbai, Boriwali")
+"""
+{'name': 'Mohan', 'email': 'mohan@gmail.com', 'phone': 4534543, 'address': 'Mumbai, Boriwali'}
+name : Mohan
+email : mohan@gmail.com
+phone : 4534543
+address : Mumbai, Boriwali
+"""
+
+
+def login(**kwargs):
+    db_username = 'user1'
+    db_password = 'user@1234'
+    if kwargs['username'] == db_username and kwargs['password'] == db_password:
+        print("Login Successfully")
+    else:
+        print("Invalid Credentials")
+
+
+print("_"*50)
+login(username='user2', password='user@123') # Invalid Credentials
+login(username='user1', password='user@1234') # Login Successfully
+
+print("_"*50)
+#############################################################
+# Recursion function:  when a function call itself, then it is called recursion function
+
+def greeting(msg):
+    print(msg)
+    greeting(msg)
+
+#greeting("Good Morning")
+# RecursionError: maximum recursion depth exceeded
+
+def print_number(num):
+    print(num, "Nidhi")
+    if num == 20:
+        return num
+    print_number(num=num+1)
+
+print_number(1)
+
+
+print("_"*50)
+####################################
+# return value of function:  If we use return keyword in the function, then it will return value from function
+# that we can store in variable and stop execution of the function immediately.
+
+def get_sum_values(n1, n2, n3):
+    return n1+n2+n3
+
+output = get_sum_values(40, 50, 60)
+print("Addition of values :", output)
+
+print("multiplication :", output*2)
+# multiplication : 300
+
+
+
+
+def print_values(num):
+    for i in range(num):
+        if i == 5:
+             return i
+        print(i)
+
+print("_"*50)
+result = print_values(10)
+print("result :", result)
+
+
+print("_"*50)
+######### return multiple value from function ##############
+# if we return multiple values from function, then it will return in form of tuple
+def math_operation(n1: int, n2: int) -> tuple:
+    add = n1+n2
+    multi = n1*n2
+    divide = n1//n2
+    return add, multi, divide
+
+
+var = math_operation(40, 5)
+print("output :", var)
+# output : (45, 200, 8)
+
+# a, b, c = (45, 200, 8)
+
+a, b, c = math_operation(50, 6)
+print("addition:", a) # addition: 56
+print("multiplication :", b) # multiplication : 300
+print("division :", c) # division : 8
+
+
+# Q1 : write a python function to check given number is prime or not return True or False value.
+# Q2 :  write a python function to get fabonacci serise till 10 values return as list
+# Q3 : Write a python function to check given string in palindrome or not, return output as True or False

877cec5803b2a408aaadab8c32e453e35d2e18b0
Deepesh <xovek11875@bitvoo.com>
2025-07-22 22:07:50 +0530

    Selenium locators

---
+++ Deepesh/SeleniumCode/selenium_locators.py
@@ -0,0 +1,73 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+
+
+class SeleniumLocators:
+    """
+    Type of locators
+
+    1. ID = "id"  # done
+    2. XPATH = "xpath"
+    3. LINK_TEXT = "link text" # done
+    4. PARTIAL_LINK_TEXT = "partial link text" # done
+    5. NAME = "name" # done
+    6. TAG_NAME = "tag name"
+    7. CLASS_NAME = "class name"
+    8. CSS_SELECTOR = "css selector"
+    """
+    def __init__(self, browser):
+        if browser.lower() == 'chrome':
+            # Launch chrome browser and initiate driver variable
+            self.driver = webdriver.Chrome()
+        elif browser.lower() == 'firefox':
+            # Launch Firefox browser and initiate driver variable
+            self.driver = webdriver.Firefox()
+        elif browser.lower() == 'edge':
+            # Launch Edge browser and initiate driver variable
+            self.driver = webdriver.Edge()
+
+        # wait for element
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+
+
+    def facebook_website(self):
+        self.driver.get("https://www.facebook.com/")
+
+        # get element with ID locator
+        self.driver.find_element(By.ID, "email").send_keys("user1@gmail.com")
+
+        # get element with Name locator
+        self.driver.find_element(By.NAME, "pass").send_keys("user@1234")
+
+        time.sleep(5)
+
+        # get element with link_text locator
+        self.driver.find_element(By.LINK_TEXT, "Create new account").click()
+
+        time.sleep(5)
+        # get element with partial_link_text locator
+        self.driver.find_element(By.PARTIAL_LINK_TEXT, "Already have").click()
+
+        time.sleep(5)
+        # get element with tagname locator
+        all_images = self.driver.find_elements(By.TAG_NAME, "img")
+        for image in all_images:
+            print(image.get_attribute("alt"))
+
+        time.sleep(5)
+        self.driver.close()
+
+    def greeting(self):
+        print("Hello good morning")
+
+
+
+
+obj = SeleniumLocators("EDGE")
+obj.facebook_website()
+obj.greeting()

15bb3914c22a7ba82dbdf88702e90ee0569b30cb
shameema1-job <shameemajob1@gmail.com>
2025-08-11 22:00:00 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Shameema/Selenium_Practice/Orange HRM/09082025.py
@@ -0,0 +1,63 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.select import Select
+
+
+# from 9082025 locators import *
+
+class sle_menthods:
+
+    def __init__(self):
+        self.driver = webdriver.Chrome( )
+        self.wait = WebDriverWait(self.driver, 10)
+        self.driver.maximize_window( )
+        self.driver.implicitly_wait(10)
+
+    def url_launch(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        time.sleep(10)
+
+    def get_element(self, locator, cond=ec.visibility_of_element_located):
+        element = self.wait.until(cond(locator))
+        return element
+
+    def click_element(self, locator, **kwargs):
+        self.get_element(locator=locator, **kwargs).click( )
+
+    def enter_data(self, locator, value, **kwargs):
+        self.get_element(locator=locator, **kwargs).send_keys(value)
+
+    def Login(self):
+        self.enter_data(locator=(By.NAME, "username"), value='Admin')
+        self.enter_data(locator=(By.NAME, 'password'), value='admin123')
+        self.click_element(locator=(By.XPATH, "//button[@type='submit']"))
+        time.sleep(10)
+
+    def Admin(self):
+        # self.click_element(By.XPATH, "//a[@text()='Admin']")
+        self.click_element(locator =(By.XPATH,"//span[contains(.,'Admin')]//parent::a[@class='oxd-main-menu-item']"))
+        time.sleep(10)
+        self.enter_data(locator=(By.XPATH,"//label[contains(.,'Username')]//parent::input"),value='Rumeha')
+        time.sleep(10)
+
+    # def forgot_password(self):
+    #     self.enter_data(locator=(By.NAME,'username'),value='Admin')
+    #     # self.click_element(locator=(By.CLASS_NAME,'oxd-text oxd-text--p orangehrm-login-forgot-header'))
+    #     # self.enter_data(locator=(By.NAME, 'password'), value='123')
+    #     self.click_element(locator=(By.XPATH,"//*[@type = 'submit']"),cond = ec.element_to_be_clickable)
+    #     time.sleep(20)
+    #     self.click_element(locator=(By.XPATH,"//button[contains(@class,'oxd-button--large')and (@type ='button')]"))
+
+
+#once user name is entered, password is changing to required field hence forgotten password is not clicked.
+
+
+obj = sle_menthods( )
+obj.url_launch( )
+obj.Login( )
+obj.Admin()
+# obj.forgot_password()
---
+++ Shameema/Selenium_Practice/Orange HRM/10082925_FBlogjn.py
@@ -0,0 +1,68 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support import expected_conditions as ec
+
+
+class Fblogin:
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver,15)
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+
+    def get_ele(self,locator,cond=ec.visibility_of_element_located):
+        element = self.wait.until(cond(locator))
+        return element
+
+    def click_ele(self,locator, **kwargs):
+        self.get_ele(locator=locator,**kwargs).click()
+
+    def enter_data(self, locator, value, **kwargs):
+        self.get_ele(locator=locator,**kwargs).send_keys(value)
+
+    def sel_dropele(self,drop_down_locator):
+        drop_down_element = self.get_ele(locator=drop_down_locator)
+        select_obj = Select(drop_down_element)
+        return select_obj
+    def url_launch(self):
+        self.driver.get("https://www.facebook.com")
+
+    # def fbLogin(self):
+    #     self.enter_data(locator=(By.ID,'email'),value="abc")
+    #     self.enter_data(locator=(By.ID,'pass'),value='123')
+    #     self.click_ele(locator=(By.NAME,"//button[@name='login']"),cond=ec.element_to_be_clickable)
+    #     time.sleep(20)
+    # que: login button is not clicked
+    def create_ac(self):
+        self.click_ele(locator=(By.XPATH," // a[ @data-testid='open-registration-form-button']"))
+        self.enter_data(locator=(By.XPATH,"//input[@name='firstname']"),value='Renga')
+        self.enter_data(locator=(By.XPATH,"//input[@name='lastname']"),value='Nayagi')
+        self.click_ele(locator =(By.XPATH,"//select[@id='day']"))
+        self.click_ele(locator=(By.XPATH, "//option[text()='1']"))
+        self.click_ele(locator=(By.XPATH, "//select[@id='month']"))
+        self.click_ele(locator=(By.XPATH, "//option[text()='Jan']"))
+        self.click_ele(locator=(By.XPATH, "//select[@id='year']"))
+        self.click_ele(locator=(By.XPATH, "//option[text()='2000']"))
+        self.click_ele(locator=(By.XPATH, "//label[text()='Female']"))
+        time.sleep(5)
+        self.enter_data(locator=(By.XPATH, "//div[text( )='Mobile number or email address']"),value='testemail@email.com',cond=ec.element_to_be_clickable)
+        time.sleep(10)
+        self.enter_data(locator=(By.NAME,'reg_passwd__'),value='Rock*135')
+        self.click_ele(locator=(By.NAME,'websubmit'))
+        time.sleep(10)
+        # self.sel_dropele(locator=(By.XPATH,"//option[text()='1']"),value='1')/\
+# que: email id field not working, able to enter password
+
+
+
+
+obj = Fblogin()
+obj.url_launch()
+# obj.fbLogin()
+obj.create_ac()
+

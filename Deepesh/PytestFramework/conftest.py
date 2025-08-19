a630997428f18e179430c8d01f22b48e96c3d3c2
Deepesh <xovek11875@bitvoo.com>
2025-08-19 22:02:12 +0530

    Selenium Methods

---
+++ Deepesh/PytestFramework/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+@pytest.fixture(scope="class")
+def class_fixture():
+    print("----- Welcome to class test execution------")
+    yield
+    print("----- class test execution completed------")

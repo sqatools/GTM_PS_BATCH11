ef2de73fe25346dd78170f37097a0652c57b539d
Deepesh <xovek11875@bitvoo.com>
2025-08-21 22:02:36 +0530

    demo framework design

---
+++ Deepesh/demo_framework/login_page_locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+username_fields = (By.NAME, "username")
+password_fields = (By.NAME, "password")
+login_button = (By.XPATH, "//button[normalize-space()='Login'] ")

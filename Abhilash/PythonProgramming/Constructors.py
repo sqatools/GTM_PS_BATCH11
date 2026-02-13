024bbaa5369f14fbae12928f25582e3fd06f70ed
ABHILASH BORRA <Abhilash113131@gmail.com>
2026-02-13 06:18:13 +0530

    modified

--- Abhilash/PythonProgramming/Constructors.py
+++ Abhilash/PythonProgramming/Constructors.py
@@ -36,22 +36,7 @@
 # print(grade)   # Fail Pass
 
 
-# Create a parent class Animal with an __init__ that takes name
-# Add a method speak that prints the name
-# Create a child class Dog that inherits from Animal
-# Create an object d1 = Dog("Rex")
-# Call d1.speak()
 
-class Animal:
-    def __init__(self,name):
-        self.name=name
-    def speak(self):
-            print(self.name)
-class Dog(Animal):
-    pass
-
-d1= Dog("Rex")
-d1.speak()
 
 
 
